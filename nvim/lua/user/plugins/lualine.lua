8ee1e2087a8a94c8214597a0138a586f849a31ff
Carl Weis <carl@carlweis.com>
2023-08-28 21:34:15 -0400

    Updated configs

--- nvim/lua/user/plugins/lualine.lua
+++ nvim/lua/user/plugins/lualine.lua
@@ -1,4 +1,4 @@
-local separator = { '"▏"', color = '#21272e' }
+local separator = { '"', color = 'StatusLineNonText' }
 
 require('lualine').setup({
   options = {
@@ -6,65 +6,42 @@ require('lualine').setup({
     component_separators = '',
     globalstatus = true,
     theme = {
-      -- normal = {
-      --   a = 'StatusLine',
-      --   b = 'StatusLine',
-      --   c = 'StatusLine',
-      -- },
       normal = {
-        a = { fg='#adbac7', bg='#1e2329', gui='' },
-        b = { fg='#adbac7', bg='#1e2329', gui='' },
-        c = { fg='#adbac7', bg='#1e2329', gui='' },
-      },
-      insert = {
-        a = { fg='#1e2329', bg='#57ab5a', gui='' },
-        b = { fg='#adbac7', bg='#1e2329', gui='' },
-        c = { fg='#adbac7', bg='#1e2329', gui='' },
-      },
-      visual = {
-        a = { fg='#1e2329', bg='#4378b8', gui='' },
-        b = { fg='#adbac7', bg='#1e2329', gui='' },
-        c = { fg='#adbac7', bg='#1e2329', gui='' },
-      },
-      command = {
-        a = { fg='#1e2329', bg='#c59126', gui='' },
-        b = { fg='#adbac7', bg='#1e2329', gui='' },
-        c = { fg='#adbac7', bg='#1e2329', gui='' },
+        a = 'StatusLine',
+        b = 'StatusLine',
+        c = 'StatusLine',
       },
     },
   },
   sections = {
     lualine_a = {
       'mode',
-      separator = nil,
+      separator,
     },
     lualine_b = {
       'branch',
       'diff',
-      separator = nil,
-      '" " .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
+      separator,
+      '"" .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
       { 'diagnostics', sources = { 'nvim_diagnostic' } },
-      separator = nil,
+      separator,
     },
     lualine_c = {
-      separator = nil,
       'filename'
     },
     lualine_x = {
-      separator = nil,
       'filetype',
       'encoding',
       'fileformat',
     },
     lualine_y = {
-      separator = nil,
+      separator,
       '(vim.bo.expandtab and "␠ " or "⇥ ") .. " " .. vim.bo.shiftwidth',
-      separator = nil,
+      separator,
     },
     lualine_z = {
       'location',
       'progress',
-      separator = nil,
     },
   },
 })
