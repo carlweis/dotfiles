ae68a4abc8beda3818b3832efc5d1190a446ee62
Carl Weis <carl@carlweis.com>
2023-08-28 22:26:05 -0400

    Fix theme

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -30,8 +30,9 @@ use('wbthomason/packer.nvim')
 -- One Dark theme.
 use({
   'jessarcher/onedark.nvim',
+
   config = function()
-    vim.cmd('colorscheme onedark')
+    vim.cmd('colorscheme habamax')
 
     vim.api.nvim_set_hl(0, 'FloatBorder', {
       fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
