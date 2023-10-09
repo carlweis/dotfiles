75b8698507a06d0fc4232bb8285a827ece5e5a81
Carl Weis <carl@carlweis.com>
2023-10-09 10:44:35 -0400

    Update colorscheme to habamax.nvim

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -39,7 +39,7 @@ use({
     'ntk148v/habamax.nvim',
     requires = { 'rktjmp/lush.nvim'},
     config = function()
-      vim.cmd('colorscheme habamax')
+      vim.cmd('colorscheme habamax.nvim')
       vim.api.nvim_set_hl(0, 'FloatBorder', {
           fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
           bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
