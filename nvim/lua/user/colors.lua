c1e2ce4626f6ea2453c0a2ac442f9bf65da676dc
Carl Weis <carl@carlweis.com>
2023-11-25 16:20:54 -0500

    Color and font updates

--- nvim/lua/user/colors.lua
+++ nvim/lua/user/colors.lua
@@ -5,3 +5,6 @@ vim.cmd('autocmd ColorScheme * highlight PMenu guibg=#232634 guifg=#ADBAC7')
 -- Splits
 vim.cmd('highlight VertSplit guibg=#232634 guifg=#232634')
 vim.cmd('highlight NormalNC guibg=#232634 guifg=#232634')
+
+-- Don't dim inactive panes/windows
+vim.cmd ('highlight Normal guibg=NONE')
