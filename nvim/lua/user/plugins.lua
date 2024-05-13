eeb4ee06784bd7270729a6eb8d3311c2f43456e5
Carl Weis <carl@carlweis.com>
2024-05-13 16:38:17 -0400

    Remove PHP plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -143,17 +143,6 @@ use({
   end,
 })
 
--- PHP Refactoring Tools
-use({
-  'phpactor/phpactor',
-  ft = 'php',
-  run = 'composer install --no-dev --optimize-autoloader',
-  config = function()
-    vim.keymap.set('n', '<Leader>pm', ':PhpactorContextMenu<CR>')
-    vim.keymap.set('n', '<Leader>pn', ':PhpactorClassNew<CR>')
-  end,
-})
-
 -- Project Configuration.
 use({
   'tpope/vim-projectionist',
