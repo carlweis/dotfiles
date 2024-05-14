8a3d474bb499763f3110f1b459b556db95961d87
Carl Weis <carl@carlweis.com>
2024-05-13 21:40:26 -0400

    Add transparency

--- nvim/lua/user/commands.lua
+++ nvim/lua/user/commands.lua
@@ -1,4 +1,11 @@
 -- strip trailing whitespace on save
+--
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        vim.cmd("hi Normal guibg=none ctermbg=none")
+    end
+})
+
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
   command = [[%s/\s\+$//e]],
--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -10,6 +10,18 @@ vim.api.nvim_set_hl(0, "TelescopeSelection", {bg="#303030", fg="#adbac7"})
 vim.api.nvim_set_hl(0, "TelescopePreviewTitle", {bg="#212121", fg="#212121"})
 vim.api.nvim_set_hl(0, "TelescopeResultsTitle", {bg="#212121", fg="#212121"})
 
+
+-- Transparency
+vim.api.nvim_set_hl(0, "TelescopeNormal", {bg="none", fg="#212121"})
+vim.api.nvim_set_hl(0, "TelescopeBorder", {bg="none", fg="#212121"})
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", {bg="none", fg="#212121"})
+vim.api.nvim_set_hl(0, "TelescopePromptNormal", {bg="none", fg="#adbac7"})
+vim.api.nvim_set_hl(0, "TelescopePromptTitle", {bg="none", fg="#adbac7"})
+vim.api.nvim_set_hl(0, "TelescopeResultsNormal", {bg="none", fg="#adbac7"})
+vim.api.nvim_set_hl(0, "TelescopeSelection", {bg="#303030", fg="#adbac7", blend=60})
+vim.api.nvim_set_hl(0, "TelescopePreviewTitle", {bg="none", fg="#212121"})
+vim.api.nvim_set_hl(0, "TelescopeResultsTitle", {bg="none", fg="#212121"})
+
 require('telescope').setup({
   defaults = {
     path_display = { truncate = 1 },
