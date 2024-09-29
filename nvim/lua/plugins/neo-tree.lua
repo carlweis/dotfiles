bc064c22ddcf3fe748684cbdfbd53518792354b2
Carl Weis <carl@carlweis.com>
2024-09-28 20:51:06 -0400

    Remove unnecessary highlights for neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -24,16 +24,6 @@ return {
             buftype = { 'terminal', "quickfix" },
           },
         },
-        highlights = {
-          statusline = {
-            focused = {
-              bg = '#9d7cd8',
-            },
-            unfocused = {
-              bg = '#9d7cd8',
-            },
-          },
-        },
       },
     },
   },
