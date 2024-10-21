841dfee91400b205cac46251a9d4198091e955b6
Carl Weis <carl@carlweis.com>
2024-10-21 12:49:44 -0400

    Catppuccin

--- nvim/lua/plugins/dracula.lua
+++ nvim/lua/plugins/dracula.lua
@@ -1,38 +1,38 @@
 return {
-  "Mofiqul/dracula.nvim",
-  config = function()
-    require("dracula").setup({
-       colors = {
-          bg = "#242424",
-          fg = "#F8F8F2",
-          selection = "#44475A",
-          comment = "#6272A4",
-          red = "#FF5555",
-          orange = "#FFB86C",
-          yellow = "#F1FA8C",
-          green = "#50fa7b",
-          purple = "#BD93F9",
-          cyan = "#8BE9FD",
-          pink = "#FF79C6",
-          bright_red = "#FF6E6E",
-          bright_green = "#69FF94",
-          bright_yellow = "#FFFFA5",
-          bright_blue = "#D6ACFF",
-          bright_magenta = "#FF92DF",
-          bright_cyan = "#A4FFFF",
-          bright_white = "#FFFFFF",
-          menu = "#252526",
-          visual = "#3E4452",
-          gutter_fg = "#4B5263",
-          nontext = "#3B4048",
-          white = "#ABB2BF",
-          black = "#191A21",
-        },
-    })
+  -- "Mofiqul/dracula.nvim",
+  -- config = function()
+  --   require("dracula").setup({
+  --      colors = {
+  --         bg = "#242424",
+  --         fg = "#F8F8F2",
+  --         selection = "#44475A",
+  --         comment = "#6272A4",
+  --         red = "#FF5555",
+  --         orange = "#FFB86C",
+  --         yellow = "#F1FA8C",
+  --         green = "#50fa7b",
+  --         purple = "#BD93F9",
+  --         cyan = "#8BE9FD",
+  --         pink = "#FF79C6",
+  --         bright_red = "#FF6E6E",
+  --         bright_green = "#69FF94",
+  --         bright_yellow = "#FFFFA5",
+  --         bright_blue = "#D6ACFF",
+  --         bright_magenta = "#FF92DF",
+  --         bright_cyan = "#A4FFFF",
+  --         bright_white = "#FFFFFF",
+  --         menu = "#252526",
+  --         visual = "#3E4452",
+  --         gutter_fg = "#4B5263",
+  --         nontext = "#3B4048",
+  --         white = "#ABB2BF",
+  --         black = "#191A21",
+  --       },
+  --   })
 
-    vim.cmd("colorscheme dracula")
+  --   vim.cmd("colorscheme dracula")
 
-    vim.api.nvim_set_hl(0, "VertSplit",    { fg = "#303030", bg = "none" })      -- Vertical split line color
-    vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#303030", bg = "none" })      -- Window separator for newer Neovim versions
-  end,
+  --   vim.api.nvim_set_hl(0, "VertSplit",    { fg = "#303030", bg = "none" })      -- Vertical split line color
+  --   vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#303030", bg = "none" })      -- Window separator for newer Neovim versions
+  -- end,
 }
